ac2dae80077d4539d67204bc9486e2f2846c80c7
sduzair <sduzairahmed@gmail.com>
2022-08-15 21:45:53 -0400

    first commit

---
+++ admin/connection.inc.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+$con = mysqli_connect("localhost", "root", "", "ecomm");
+define('SERVER_PATH', $_SERVER['DOCUMENT_ROOT'] . '/spring22-project/main/');
+define('SITE_PATH', 'http://127.0.0.1/spring22-project/main/');
+
+define('PRODUCT_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product/');
+define('PRODUCT_IMAGE_SITE_PATH', SITE_PATH . 'media/product/');
+
+define('PRODUCT_MULTIPLE_IMAGE_SERVER_PATH', SERVER_PATH . 'media/product_images/');
+define('PRODUCT_MULTIPLE_IMAGE_SITE_PATH', SITE_PATH . 'media/product_images/');
+
+define('BANNER_SERVER_PATH', SERVER_PATH . 'media/banner/');
+define('BANNER_SITE_PATH', SITE_PATH . 'media/banner/');
+
+define('SHIPROCKET_TOKEN_EMAIL', 'gmail');
+define('SHIPROCKET_TOKEN_PASSWORD', 'password');
